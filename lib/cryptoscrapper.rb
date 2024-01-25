17b956ebfbdb51182ed4d5c20833fe061f565ef3
splmloic <l2ct@protonmail.com>
2024-01-25 20:06:50 +0100

    rendu final

--- lib/cryptoscrapper.rb
+++ lib/cryptoscrapper.rb
@@ -7,39 +7,28 @@ def parse
 end
 
 def scrape (page)
+    crypto_index = []
     #chemin Xpath du nom des cryptos
     all_crypto_name = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div')
     #chemin Xpath du prix des crypto
     all_crypto_price = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div/a/span')
 
-    # je crée mon hash
-    crypto_data = {}
-
-    #j'emboite les prix avec les noms dans mon hash
-    all_crypto_name.each_with_index do |name, index|
+    #j'emboite les prix avec les noms dans un hash
+    all_crypto_name.each_with_index do |name,index|
         price = all_crypto_price[index]
-        crypto_data[name.text] = price.text
+        crypto_data = {
+            name.text => price.text.delete_prefix("$").delete(",").to_f
+        }
+        #puis j'envoie les hash dans mon array
+        crypto_index << crypto_data 
     end
-
-    return crypto_data
-end
-
-def output(crypto_data)
-    #je transforme mon hash en une array de plusieurs petit hash
-    crypto_index = []
-
-    crypto_data.each do |k, v|
-        puts "#{k} => #{v.delete_prefix("$").delete(",").to_f}"
-    end
+    return crypto_index
 end
 
 def perform
     page = parse
-    crypto_data = scrape (page)
-    output(crypto_data)
+    crypto_index = scrape (page)
+    puts crypto_index
 end
 
 perform
-
-#une array de hash pas hash de array
-#{ "BTC" => 5245.12 } un hash ?

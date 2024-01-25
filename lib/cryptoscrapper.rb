3c1a14d5ffe733ae1c9bfb4fe2d904f84b1a873e
splmloic <l2ct@protonmail.com>
2024-01-25 15:10:30 +0100

    exo1

--- lib/cryptoscrapper.rb
+++ lib/cryptoscrapper.rb
@@ -1,25 +1,45 @@
 require 'nokogiri'
 require 'open-uri'
-result = Hash.new
-page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
 
-all_crypto_name = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div')
-all_crypto_price = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div/a/span')
+def parse
+    #je lance la recherche de la page
+    page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+end
 
-puts "#{all_crypto_name.length}"
-puts "#{all_crypto_price.length}"
-for 
-all_crypto_name.each do |name|
-    puts name.text #ou n'importe quelle autre opération de ton choix ;)
-    result[0] = name.text
-end
-all_crypto_price.each do |price|
-    puts price.text
-    result[0] = price.text
-end
-puts result
+def scrape (page)
+    #chemin Xpath du nom des cryptos
+    all_crypto_name = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div')
+    #chemin Xpath du prix des crypto
+    all_crypto_price = page.xpath('//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div/a/span')
 
-all_crypto_name.length.times do |i|
-    result[all_crypto_name[i]] = all_crypto_price[i]
+    # je crée mon hash
+    crypto_data = {}
+
+    #j'emboite les prix avec les noms dans mon hash
+    all_crypto_name.each_with_index do |name, index|
+        price = all_crypto_price[index]
+        crypto_data[name.text] = price.text
+    end
+
+    return crypto_data
 end
-puts result
+
+def output(crypto_data)
+    #je transforme mon hash en une array de plusieurs petit hash
+    crypto_index = []
+
+    crypto_data.each do |k, v|
+        puts "#{k} => #{v.delete_prefix("$").delete(",").to_f}"
+    end
+end
+
+def perform
+    page = parse
+    crypto_data = scrape (page)
+    output(crypto_data)
+end
+
+perform
+
+#une array de hash pas hash de array
+#{ "BTC" => 5245.12 } un hash ?
---
+++ spec/cryptoscrapper_test.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/cryptoscrapper'
+
+describe "scrape value" do
+    it "should return a value not nil" do
+      expect(output()).to eq(output to some stream)
+    end
+end
+

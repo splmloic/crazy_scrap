070a46d9c29bb3edaeb4b1e9f1cbb18386f16996
splmloic <l2ct@protonmail.com>
2024-01-26 14:54:30 +0100

    minor fix

--- lib/depute.rb
+++ lib/depute.rb
@@ -14,7 +14,7 @@ end
 
 def get_depute_data (depute_url)
     depute_data = []
-
+begin
     depute_url.length.times do |i|
         dep = Nokogiri::HTML(URI.open"#{"https://www2.assemblee-nationale.fr"+depute_url[i]}")
         depute_mail = dep.xpath('//*[@id="main"]/div/div/div/section[2]/div/ul/li[1]/a/span')
@@ -22,16 +22,16 @@ def get_depute_data (depute_url)
         depute_prenom = dep.xpath('//*[@id="main"]/div/div/div/section[1]/div/div[1]/h1')
 
         depute_data_hash = {
-            "prenom" => ((depute_prenom.text).match(/(M\.|Mme) (\w+)/)),
-            "nom" => ((depute_nom.text).match(/(M\.|Mme) \w+ (\w+)/)),
-            "mail" => depute_mail.text
-        }
+            "First Name" => ((depute_prenom.text).match(/(M\.|Mme) (\w+)/))[2],
+            "Last Name" => ((depute_nom.text).match(/(M\.|Mme) \w+ (\w+)/))[2], 
+            "Mail" => depute_mail.text
+        } rescue NoMethodError => e
+
         puts depute_data_hash    
         depute_data << depute_data_hash
     end
+end
     puts depute_data
 end
 depute_url = get_depute_url
 get_depute_data (depute_url)
-
-#j'arrete la je vais dodo mais merlish ça passe
